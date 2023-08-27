77898ed75ee56c3c8d91619eb60fdd851ddb0d63
Alexandra Kostrova <sashabravos@gmail.com>
2023-08-27 17:31:30 +0300

    Homework complete

--- M18Homework/Kinobox/Kinobox/Network/NetworkManager.swift
+++ M18Homework/Kinobox/Kinobox/Network/NetworkManager.swift
@@ -8,12 +8,6 @@
 import Alamofire
 import UIKit
 
-// MARK: - RequestResultDelegate
-
-protocol RequestResultDelegate: AnyObject {
-    func getResult(_ result: String)
-}
-
 final class RequestManager {
     
     // MARK: - Instants
@@ -21,56 +15,102 @@ final class RequestManager {
     static let shared = RequestManager()
     private init() {}
     
-    weak var delegate: RequestResultDelegate?
-    
     private lazy var apiKey = Constants.Keys.apiKey
-    private lazy var baseURL =
-    "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword"
+    private lazy var filmsURL = Constants.Link.filmsURL
+    private lazy var detailURL = Constants.Link.detailURL
+    private lazy var searchByKeywordURL = Constants.Link.searchByKeywordURL
+    private lazy var topFilmsURL = Constants.Link.topFilmsURL
     
     // MARK: - Public methods
     
-    public func searchMovies(by keyword: String, with method: RequestButton.RequestType) {
-        var components = URLComponents(string: baseURL)
+    public func searchMovies<T: Decodable>(by keyword: String, completion: @escaping (Result<T, Error>) -> Void) {
+        var components = URLComponents(string: searchByKeywordURL)
         components?.queryItems = [URLQueryItem(name: "keyword", value: keyword)]
         
         guard let url = components?.url else {
-            self.delegate?.getResult("Something went wrong... \nCheck your URL")
+            print("Something went wrong... \nCheck your URL")
             return
         }
         
-        switch method {
-        case .alamofire:
-            let headers: HTTPHeaders = [
-                "X-API-KEY": apiKey
-            ]
-            
-            AF.request(url, method: .get, headers: headers).responseData { response in
-                switch response.result {
-                case .success(let data):
-                    if let convertedString = String(data: data, encoding: .utf8) {
-                        self.delegate?.getResult(convertedString)
-                    } else {
-                        self.delegate?.getResult("Something went wrong...\nCheck your Data type")
-                    }
-                case .failure(let error):
-                    print("Error:\n \(error)")
+        let headers: HTTPHeaders = [
+            "X-API-KEY": apiKey
+        ]
+        
+        AF.request(url, method: .get, headers: headers).responseData { response in
+            switch response.result {
+            case .success(let data):
+                do {
+                    let decodedData = try self.decodeData(T.self, from: data)
+                    completion(.success(decodedData))
+                } catch {
+                    completion(.failure(error))
                 }
+            case .failure(let error):
+                completion(.failure(error))
             }
-            
-        case .urlSession:
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            request.addValue(apiKey, forHTTPHeaderField: "X-API-KEY")
-            
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error = error {
-                    print("Error:\n \(error)")
-                } else if let data = data {
-                    let convertedString = String(data: data, encoding: .utf8)
-                    self.delegate?.getResult(convertedString ?? "Something went wrong...\nCheck your Data type")
-                }
-            }
-            task.resume()
         }
+    }
+    
+    public func getTopMovies<T: Decodable>(completion: @escaping (Result<T, Error>) -> Void) {
+        var components = URLComponents(string: topFilmsURL)
+        components?.queryItems = [URLQueryItem(name: "type", value: "TOP_100_POPULAR_FILMS")]
+        
+        guard let url = components?.url else {
+            print("Something went wrong... \nCheck your URL")
+            return
+        }
+        
+        let headers: HTTPHeaders = [
+            "X-API-KEY": apiKey
+        ]
+        
+        AF.request(url, method: .get, headers: headers).responseData { response in
+            switch response.result {
+            case .success(let data):
+                do {
+                    let decodedData = try self.decodeData(T.self, from: data)
+                    completion(.success(decodedData))
+                } catch {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    public func getFilmInfo(id: Int, completion: @escaping (Result<FilmInfo, Error>) -> Void) {
+        let components = URLComponents(string: detailURL + "\(id)")
+        guard let url = components?.url else {
+            print("Something went wrong... \nCheck your URL")
+            return
+        }
+        
+        let headers: HTTPHeaders = [
+            "X-API-KEY": apiKey
+        ]
+        
+        AF.request(url, method: .get, headers: headers).responseData { response in
+            switch response.result {
+            case .success(let data):
+                do {
+                    let decodedData = try self.decodeData(FilmInfo.self, from: data)
+                    completion(.success(decodedData))
+                } catch {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+    
+    // MARK: - Private methods
+    
+    /// Universal function to decode
+    private func decodeData<T: Decodable>(_ type: T.Type, from data: Data) throws -> T {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
     }
 }
